73648b0bd3e424b1552f65d0362fd100671ff3f3
Zakaria Yaris <info@zakariya.me>
2022-11-07 19:39:39 +0000

    ADD: dark/light mode, hmburgr menu & media queries

--- script.js
+++ script.js
@@ -13,6 +13,42 @@ projects.forEach((description, ind) => {
   });
 });
 
+// Dark/Light mode functionality section
+
+// i don't know why the condition is working in this way
+// and not the reverse (the correct way)
+let darkMode = true;
+const visibilityButton = document.querySelector('.visibility');
+
+visibilityButton.addEventListener('click', () => {
+  if (darkMode) {
+    visibilityButton.innerHTML = '<i class="uil uil-sun"></i>';
+    document.querySelector('body').style.backgroundColor = '#2b2d3e';
+    document.querySelector('body').style.color = '#2b2d3e';
+    darkMode = false;
+  } else {
+    visibilityButton.innerHTML = '<i class="uil uil-moon"></i>';
+    document.querySelector('body').style.backgroundColor = '#fff';
+    document.querySelector('body').style.color = '#2b2d3e';
+    darkMode = true;
+  }
+});
+
+// Hamburger menu functionality section
+let menuIsVisible = false;
+const barsButton = document.querySelector('.bars-button');
+const hamburgerMenu = document.querySelector('.menu');
+
+barsButton.addEventListener('click', () => {
+  if (menuIsVisible) {
+    hamburgerMenu.style.display = 'none';
+    menuIsVisible = false;
+  } else {
+    hamburgerMenu.style.display = 'block';
+    menuIsVisible = true;
+  }
+});
+
 // memeory game message display logic setup
 // for (let i = 1; i <= 3; i++) {
 //   setTimeout(() => {
